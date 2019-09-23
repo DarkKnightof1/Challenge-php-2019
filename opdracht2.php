4d7a90b23b9c702eb53c264de66bb594766ef62c
DarkKnightof1 <mischa@boskat.eu>
2019-09-23 09:04:00 +0200

    Opdracht 2

---
+++ opdracht2.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: misch
+ * Date: 23-9-2019
+ * Time: 08:59
+ */
+
+$boodschap = "aardappel";
+$a = "Ik moet vandaag " . $boodschap . "en kopen.";
+echo $a;
