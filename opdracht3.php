9d40568a5da55eaa5e1ce8c715752262e20451ca
DarkKnightof1 <mischa@boskat.eu>
2019-09-23 09:58:05 +0200

    Opdracht 3 bonus

--- opdracht3.php
+++ opdracht3.php
@@ -7,9 +7,11 @@
  */
 
 $boodschappen = ["aardappelen", "aardbeien", "3 pakken melk", "yoghurt"];
+$boodschapplaatjes = ['<img src="img/p1.jpg">', '<img src="img/p2.jpg">', '<img src="img/p3.jpg">', '<img src="img/p4.jpg">'];
 $l = count($boodschappen);
 
 for($a = 0; $a < $l; $a++) {
     echo $boodschappen[$a];
+    echo $boodschapplaatjes[$a];
     echo "<br>";
 }
