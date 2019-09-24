486e920c91415148e15cea8a51ad754c4728f89d
DarkKnightof1 <mischa@boskat.eu>
2019-09-24 16:19:33 +0200

    Opdracht 6

---
+++ opdracht5.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: misch
+ * Date: 23-9-2019
+ * Time: 12:02
+ */
+
---
+++ opdracht6.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: misch
+ * Date: 24-9-2019
+ * Time: 13:26
+ */
+
+$dbh = new PDO('mysql:host=localhost;dbname=boodschappen;port=8889', 'root', 'root');
+
+$db_host     = "localhost";
+$db_username = "root";
+$db_password = "root";
+$connection = mysqli_connect($db_host, $db_username, $db_password) or die("Error ");
+$db = mysqli_select_db($connection, "boodschappen") or die("Error ");
+
+$sql = mysqli_query($connection, "SELECT * FROM idproduct");
+
+while($row = mysqli_fetch_array($sql)) {
+    $boodschappen[] = $row['idproduct'];
+    
+    echo 'idproduct';
+}
