6c1068d024f39bf4770a68c0103c9f64751f2e60
DarkKnightof1 <mischa@boskat.eu>
2019-10-08 14:12:42 +0200

    Opdracht 6

--- opdracht6.php
+++ opdracht6.php
@@ -6,18 +6,8 @@
  * Time: 13:26
  */
 
-$dbh = new PDO('mysql:host=localhost;dbname=boodschappen;port=8889', 'root', 'root');
+$dbh = new PDO('mysql:host=localhost;dbname=boodschappen2;port=8889', 'root', 'root');
+foreach($dbh->query('SELECT * from product') as $row) {
+    print_r($row);
 
-$db_host     = "localhost";
-$db_username = "root";
-$db_password = "root";
-$connection = mysqli_connect($db_host, $db_username, $db_password) or die("Error ");
-$db = mysqli_select_db($connection, "boodschappen") or die("Error ");
-
-$sql = mysqli_query($connection, "SELECT * FROM idproduct");
-
-while($row = mysqli_fetch_array($sql)) {
-    $boodschappen[] = $row['idproduct'];
-
-    echo 'idproduct';
 }
